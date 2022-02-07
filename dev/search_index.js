99504aed2a5840d45dd6023db9efd25399b2d83a
Documenter.jl <documenter@juliadocs.github.io>
2022-02-07 06:15:08 +0000

    build based on 43002ae

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SgdlDev","category":"page"},{"location":"#SgdlDev","page":"Home","title":"SgdlDev","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SgdlDev.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SgdlDev]","category":"page"}]
+}
