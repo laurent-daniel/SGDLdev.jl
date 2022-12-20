a5ff7aff1b911e960e65a4e0dacdff56979b51ee
Laurent Daniel <laurent.daniel@sgdl.com>
2022-12-20 19:23:36 +0900

    Change test file.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,10 @@ using Test
 
 @testset "SgdlDev.jl" begin
     # Write your tests here.
+
+    @test Test01([1,2],3) == BigInt[3, 6]
+    @test Test01([1,2],BigInt(3)) == BigInt[3, 6]
+    @test Test01([BigInt(1),BigInt(2)],BigInt(3)) == BigInt[3, 6]
+
+
 end
